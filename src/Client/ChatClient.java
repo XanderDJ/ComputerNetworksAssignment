3ef3ca1e622aa3739b55d12c4892ddbb26cfd9ea
BensonDH <benson.deheel@student.kuleuven.be>
2018-03-15 16:07:47 +0100

    Progress tot nu toe, nog niet runnen want het werkt nog niet

--- src/Client/ChatClient.java
+++ src/Client/ChatClient.java
@@ -1,9 +1,7 @@
 package Client;
-import java.io.OutputStream;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.io.FileWriter;
 
 
 public class ChatClient {
@@ -18,11 +16,37 @@ public class ChatClient {
             place(command, url, port);
     }
 
+    public ChatClient(String command, String url){
+        connect(url, DEFAULTPORT);
+
+        if (command.equals("GET") || command.equals("HEAD"))
+            fetch(command, url, DEFAULTPORT);
+        else if (command.equals("PUT") || command.equals("POST"))
+            place(command, url, DEFAULTPORT);
+    }
+
+
     public void fetch(String command, String url, int port){
         try {
-            String message = "GET "+url+" "+ String.valueOf(port)+" HTTP/1.1";
-            PrintWriter writer = new PrintWriter(this.connection.getOutputStream());
-            writer.println(message);
+            outputWtr.println(command+" /index.html HTTP/1.1");
+            outputWtr.println("Host: "+url+":"+String.valueOf(port));
+            outputWtr.println("");
+            outputWtr.flush();
+
+            StringBuilder response = new StringBuilder();
+            int contentLength = 1;
+            Boolean readingContent = false;
+            StringBuilder line = new StringBuilder();
+            int currentChar;
+            while (contentLength > 0 && (currentChar = inFromServer.read()) != -1){
+                line.append(currentChar);
+
+            }
+
+
+
+
+            System.out.print(response);
 
         } catch (Exception e){
             System.out.println(e);
@@ -36,21 +60,17 @@ public class ChatClient {
     public void connect(String url, int port){
         try{
             this.connection = new Socket(url, port);
+            this.outputWtr = new PrintWriter(connection.getOutputStream());
+            this.inFromServer = new InputStreamReader(connection.getInputStream());
+
+            System.out.println("Connected to: "+url+"\n");
         } catch(Exception e){
             System.out.println(e);
         }
     }
 
-    public void connect(String url){
-        try{
-            this.connection = new Socket(url, DEFAULTPORT);
-            this.writer = new PrintWriter(this.connection.getOutputStream());
-        } catch(Exception e){
-            System.out.print(e);
-        }
-    }
 
-
-    private PrintWriter writer;
     private Socket connection;
+    private PrintWriter outputWtr;
+    private InputStreamReader inFromServer;
 }
