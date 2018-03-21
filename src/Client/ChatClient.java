61c12b1ae532694294be4cf0f332804356a7a726
BensonDH <benson.deheel@student.kuleuven.be>
2018-03-21 20:41:05 +0100

    Grote optimisatie voor het binnenhalen van server-messages door het gebruik van een BufferedReader + Groot deel van de documentatie geschreven voor de ChatClient-class

--- src/Client/ChatClient.java
+++ src/Client/ChatClient.java
@@ -1,17 +1,39 @@
 package Client;
-import com.sun.javafx.iio.ios.IosDescriptor;
 
 import java.io.*;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileWriter;
 
-
+/**
+ * Class that represents a Chat-Client.
+ *
+ * These Chat-clients can sent http requests to servers and interpret their answers.
+ *
+ * Implemented http functions:
+ * GET - Request and save a document from a server.
+ * HEAD - Request the header of a document from a server.
+ * PUT - Send a file to the server that will be stored on the server side and overwritten if the server
+ *       already has a similar named file.
+ * POST - Send a file to the server that will be appended to any existing file with the same name on
+ *        the server.
+ *
+ * Authors: De Heel Benson, De Jaegere Xander
+ * Version: 1.0
+ */
 public class ChatClient {
     private static int DEFAULTPORT = 80;
 
+    /**
+     * Initialize a new ChatClient.
+     * When invoked, the ChatClient will connect to the given DNS-adress on the given port.
+     * The constructor will automatically invoke the right http-request, depending om the given command.
+     *
+     * @param command   The http-request to invoke.
+     * @param url       The adress to connect to.
+     * @param port      The port to be used.
+     */
     public ChatClient(String command, String url, int port){
         String[] parsedURL = parseURL(url);
         connect(parsedURL[0], port);
@@ -22,10 +44,30 @@ public class ChatClient {
             place(command, parsedURL[0], parsedURL[1], port);
     }
 
+    /**
+     * Initialize a new ChatClient.
+     * This consstructor has the same effect as
+     *      ChatClient(command, url, DEFAULT_PORT)
+     *
+     * @param command   The http-request to invoke.
+     * @param url       The adress to connect to.
+     */
     public ChatClient(String command, String url){
         this(command, url, DEFAULTPORT);
     }
 
+
+    /**
+     * Parse the given url by cutting off the "http://" and "http://" parts and splitting them
+     * into:
+     * - a DNS adress, for example www.example.com
+     * - a File name/File location on the requested server, for example /index.html
+     *
+     * @param url      The URL to be parsed.
+     * @return         An array containging the parsed URL with:
+     *                      result[0]: DNS-adress
+     *                      result[1]: File name/File location
+     */
     private String[] parseURL(String url){
         if( url.contains("https://")){
             url = url.substring(8);
@@ -35,102 +77,127 @@ public class ChatClient {
         return url.split("/", 2);
     }
 
-    public void fetch(String command, String dnsAdress, String location, int port){
+
+    /**
+     * This method represents the "GET" and "HEADER" http-method.
+     *
+     * If command == "Get" this method will:
+     *      - Send an http-GET request for the file "fileName" to the open connection trough the given port
+     *      - Retreive, interpret and print out the header-response of the server
+     *      - If the request was sucessful, this method will invoke a helper method to save the retreived
+     *        file
+     *
+     *  IF command == "HEADER" this method will:
+     *      - Send an http-HEADER reques for the file "fileName" to the open connecton trough the given port
+     *      - Retreive and print out the header-response of the server
+     *
+     * @param command   A String which has to be "GET" or "HEADER", depending on the http-request
+     * @param dnsAdress The host-adress where the File/Header has to be retreived.
+     * @param fileName  The name of the file that has to be retreived.
+     * @param port      The port to use.
+     */
+    public void fetch(String command, String dnsAdress, String fileName, int port){
         try {
             // --- Send request
-            outputWtr.println(command+" /"+location+" HTTP/1.1");
+            outputWtr.println(command+" /"+fileName+" HTTP/1.1");
             outputWtr.println("Host: "+dnsAdress+":"+String.valueOf(port));
             outputWtr.println("");
             outputWtr.flush();
 
             // --- Receive the header
-            StringBuilder header = new StringBuilder();
-            StringBuilder line = new StringBuilder();
-            int currentChar;
+            //StringBuilder header = new StringBuilder();
+            String line;
             int contentLength = -1;
-            while ((currentChar = inFromServer.read()) != -1) {
-                line.append(Character.toChars(currentChar));
+            while ((line = inFromServer.readLine()) != null) {
+                System.out.println(line);
+                //header.append(line);
+                //header.append("\r\n");
 
-                // De lijn is volledig
-                if (currentChar == '\n') {
-                    header.append(line);
-
-                    if (line.toString().contains("Content-Length:")) {
-                        contentLength = Integer.valueOf(line.toString().trim().split(" ")[1]);
-                    } else if (line.toString().equals("\r\n")) {
-                        break;
-                    }
-                    line = new StringBuilder();
+                if (line.contains("Content-Length:")) {
+                    contentLength = Integer.valueOf(line.trim().split(" ")[1]);
+                }
+                 else if (line.length() == 0) {
+                    break;
                 }
             }
-            System.out.println(header);
-            StringBuilder content;
-            if  (contentLength >= 0){
-                content = readData(contentLength);
-            } else
-                content = readChunked();
 
-            saveFiles(dnsAdress, location, content.toString(), ".html");
-            System.out.print(content);
+            if (command.equals("GET")) {
+                String content;
+                if (contentLength >= 0) {
+                    content = readData(contentLength);
+                } else
+                    content = readChunked();
 
+
+                saveFiles(dnsAdress, fileName.split("\\?", 2)[0], content, ".html");
+                System.out.print(content);
+            }
         } catch (Exception e){
             System.out.println(e);
         }
     }
 
-    private StringBuilder readData(int length){
+
+    /**
+     * Helper method for the fetch and readChunked methods.
+     *
+     * This method will read "length" chars from the InputStream of the connection of this ChatClient and
+     * return the result as one string.
+     *
+     * @param length    The amount of chars to be read.
+     * @return          A string read from the InputStream of the connection of this ChatClient.
+     *                  OR an empty string if an exception occured.
+     */
+    private String readData(int length) {
+        try {
+            char[] text = new char[length];
+
+            int i = inFromServer.read(text, 0, length);
+
+            return new String(text);
+
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        return "";
+    }
+
+
+    /**
+     * This method is a helper method of the method fetch.
+     *
+     * Reads a server response with "Transfer-Encoding: Chunked" from the InputStream of the connection
+     * of this ChatClient.
+     * This method will read all chunks comming in from the server until the server sends 0 as the next
+     * Chunk-length.
+     *
+     * @return A string that contains the full response from a server with Transfer-encoding: Chunked.
+     */
+    private String readChunked(){
         try {
             StringBuilder response = new StringBuilder();
-            StringBuilder line = new StringBuilder();
-            int currentChar;
-
-            while (length > 0 && (currentChar = inFromServer.read()) != -1) {
-                line.append(Character.toChars(currentChar));
-
-                // De lijn is volledig
-                if (currentChar == '\n') {
-                    response.append(line);
-                    line = new StringBuilder();
-                }
-                length -= 1;
+            String lengthStr;
+            while ((lengthStr = inFromServer.readLine()) != null && !lengthStr.equals("0")){
+                if (lengthStr.length() == 0)
+                    continue;
+                response.append(readData(Integer.valueOf(lengthStr.trim(), 16)));
             }
-            // Add the last line
-            response.append(line);
 
-            return response;
+            return response.toString();
         } catch (IOException e){
             System.out.println(e);
         }
-
-        return new StringBuilder();
+        return "";
     }
 
-    private StringBuilder readChunked(){
-        try {
-            StringBuilder response = new StringBuilder();
-            StringBuilder line = new StringBuilder();
-            int currentChar;
-            boolean ignoreNewline = false;
-            while ((currentChar = inFromServer.read()) != '0' && currentChar != -1) {
-                if (currentChar == '\n'){
-                    if (!(ignoreNewline)) {
-                        response.append(readData(Integer.valueOf(line.toString().trim(), 16)));
-                        line = new StringBuilder();
-                        ignoreNewline = true;
-                        continue;
-                    } else
-                        ignoreNewline = false;
-                }
-                line.append(Character.toChars(currentChar));
-            }
 
-            return response;
-        } catch (IOException e){
-            System.out.println(e);
-        }
-        return new StringBuilder();
-    }
-
+    /**
+     *
+     * @param command
+     * @param host
+     * @param location
+     * @param port
+     */
     public void place(String command, String host,String location, int port){
         try{
             // interative prompt from command line
@@ -193,7 +260,8 @@ public class ChatClient {
         dir.mkdirs();
         try {
             File file = new File(dir, fileName);
-            FileWriter writer = new FileWriter(file, false);
+            file.createNewFile();
+            FileWriter writer = new FileWriter(file);
             writer.append(content);
             writer.flush();
             writer.close();
